2a4b305f4c2d6355633a8fdddfa65f2d3d6e2fbc
Michael Schwarz <->
2020-03-22 16:55:35 +0100

    change content to text only + added headline

--- database/models.py
+++ database/models.py
@@ -12,9 +12,10 @@ class NewsEntry(db.Model):
     identifier = db.Column(db.String(128), primary_key=True, unique=True)
     source = db.Column(db.String(128))
     query_url = db.Column(db.String(128), nullable=False)
-    created = db.Column(db.Integer, default=timestamp)
-    last_update = db.Column(db.Integer, default=timestamp, onupdate=timestamp)
-    content = db.Column(db.JSON)
+    created = db.Column(db.DateTime, default=timestamp)
+    last_update = db.Column(db.DateTime, default=timestamp, onupdate=timestamp)
+    content = db.Column(db.String(8192))
+    headline = db.Column(db.String(1024))
     area = db.Column(db.String(128), nullable=True)
     category = db.Column(db.String, nullable=True)
     tags = db.Column(db.JSON)
@@ -22,8 +23,8 @@ class NewsEntry(db.Model):
     @staticmethod
     def serialize(ob):
         return {'source': ob.source, 'query_url': ob.query_url, 'created': ob.created,
-                'last_update': ob.last_update, 'content': ob.content, 'area': ob.area, 'category': ob.category,
-                'tags': ob.tags}
+                'last_update': ob.last_update, 'headline': ob.headline, 'content': ob.content, 'area': ob.area,
+                'category': ob.category, 'tags': ob.tags}
 
     def as_dict(self):
         return dict((col, getattr(self, col)) for col in
